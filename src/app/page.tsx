d935397235d2bb2b72e2e3a8bf284a0633d6bb2a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-01 16:20:47 +0300

    fetch events on page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import Calendar from "./components/Calendar";
+
+import Event from "@/database/models/Event";
+import connectDB from "@/database/connection";
+
+const fetchEvents = async () => {
+  await connectDB();
+  const data = await Event.find();
+
+  // Serialize data
+  const dataJSON = JSON.stringify(data);
+  return JSON.parse(dataJSON);
+};
+
+export default async function Home() {
+  const events = await fetchEvents();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
+      <Calendar events={events} />
     </main>
   );
 }
