ae69e281cd29dbb96bf7be62fc1186daa96740e1
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-10-02 10:41:53 +0300

    add a dev and production db string

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,13 @@
 import { connect } from "mongoose";
 
+const MONGO_URL: string =
+  process.env.NODE_ENV !== "production"
+    ? process.env.LOCAL_DB!
+    : process.env.MONGO_URL!;
+
 const connectDB = async () => {
   try {
-    const conn = await connect(process.env.MONGO_URL!);
+    const conn = await connect(MONGO_URL);
     console.log(`Connected to database : ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error : ${error}`);
